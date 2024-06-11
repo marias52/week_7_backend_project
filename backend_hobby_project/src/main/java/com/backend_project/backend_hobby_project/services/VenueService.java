c438414e45f9fb51ff55aa04d3800e652c3ec493
Danash Mahmood <danash.mahmoodof@gmail.com>
2024-06-11 10:24:19 +0100

    completing UserService MVP

--- backend_hobby_project/src/main/java/com/backend_project/backend_hobby_project/services/VenueService.java
+++ backend_hobby_project/src/main/java/com/backend_project/backend_hobby_project/services/VenueService.java
@@ -1,4 +1,58 @@
 package com.backend_project.backend_hobby_project.services;
 
+
+import com.backend_project.backend_hobby_project.models.Venue;
+import com.backend_project.backend_hobby_project.repositories.VenueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class VenueService {
+
+
+    @Autowired
+    VenueRepository venueRepository;
+
+
+    public List<Venue> findAllVenues(){
+        return this.venueRepository.findAll();
+    }
+
+    public Optional<Venue> findVenueById(long id){
+        return this.venueRepository.findById(id);
+    }
+
+    public void addVenue(Venue venue){
+        this.venueRepository.save(venue);
+    }
+
+    public void deleteVenueById(long id){
+        this.venueRepository.deleteById(id);
+    }
+
+    public void deleteVenue(Venue venue){
+        this.venueRepository.delete(venue);
+    }
+
+    public void updateVenue(Venue venue, long id){
+        String newLocation = venue.getLocation();
+        int newCapacity = venue.getCapacity();
+
+        Venue existingVenue = this.findVenueById(id).get();
+
+        existingVenue.setLocation(newLocation);
+        existingVenue.setCapacity(newCapacity);
+
+
+
+    }
+
+
+
+
+
+
 }
