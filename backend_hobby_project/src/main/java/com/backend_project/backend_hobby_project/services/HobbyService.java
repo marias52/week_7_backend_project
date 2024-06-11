7e1775841a4e0a3030bd0f38fdccdbeb79b7ee2d
Sunny Mudhar <sunielmudhar@gmail.com>
2024-06-11 10:56:09 +0100

    Added Hobby Service and missing .save function

--- backend_hobby_project/src/main/java/com/backend_project/backend_hobby_project/services/HobbyService.java
+++ backend_hobby_project/src/main/java/com/backend_project/backend_hobby_project/services/HobbyService.java
@@ -1,4 +1,44 @@
 package com.backend_project.backend_hobby_project.services;
 
+import com.backend_project.backend_hobby_project.models.Hobby;
+import com.backend_project.backend_hobby_project.repositories.HobbyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class HobbyService {
+
+    @Autowired
+    HobbyRepository hobbyRepository;
+
+    public void addHobby(Hobby hobby) {
+        hobbyRepository.save(hobby);
+    }
+
+    public List<Hobby> getAllHobbies() {
+        return hobbyRepository.findAll();
+    }
+
+    public Optional<Hobby> findHobbyById(Long id) {
+        return hobbyRepository.findById(id);
+    }
+
+    public void deleteHobbyById(Long id) {
+        hobbyRepository.deleteById(id);
+    }
+
+    public void deleteHobbyByEntity(Hobby hobby) {
+        hobbyRepository.delete(hobby);
+    }
+
+    public void updateHobby(Hobby hobby, Long id) {
+        String hobbyName = hobby.getName();
+        Hobby hobbyToUpdate = this.findHobbyById(id).get();
+
+        hobbyToUpdate.setName(hobbyName);
+        hobbyRepository.save(hobbyToUpdate);
+    }
 }
