a2cbbee47017bdb034919e70cca71f32caf299d1
maria <mariasharif.16@hotmail.com>
2024-06-11 10:13:10 +0100

    completed mvp user service

--- backend_hobby_project/src/main/java/com/backend_project/backend_hobby_project/services/UserService.java
+++ backend_hobby_project/src/main/java/com/backend_project/backend_hobby_project/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -20,6 +21,36 @@ public class UserService {
         return this.userRepository.findAll();
     }
 
+
+    public Optional<User> findUserById( long id) {
+        return this.userRepository.findById(id);
+    }
+
+    public void addUser (User user){
+        this.userRepository.save(user);
+    }
+
+    public void deleteUserById (long id){
+        this.userRepository.deleteById(id);
+    }
+
+    public void deleteUser (User user){
+        this.userRepository.delete(user);
+    }
+
+    public void updateUser (User user, long id){
+        String newName = user.getName();
+        int newAge = user.getAge();
+        String newLocation = user.getLocation();
+        String newBio = user.getBiography();
+
+        User existingUser = this.findUserById(id).get();
+        existingUser.setName(newName);
+        existingUser.setAge(newAge);
+        existingUser.setLocation(newLocation);
+        existingUser.setBiography(newBio);
+
+    }
 
 
 
